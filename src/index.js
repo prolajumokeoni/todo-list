a5fa20332aad44d02a3ce794b881926125843e58
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-08-19 10:07:01 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,52 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-let inputField = document.querySelector('#inputField')
-let todoList = document.querySelector('#todoList')
-
-const todos= [
-	{
-		description: 'wash my hair',
-		index: 1,
-		completed: 0
-	},
-	{
-		description: 'sleep',
-		index: 2,
-		completed: 0
-	}
-]
-inputField.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-  console.log(inputField.value)
-  todos.push({description: inputField.value,
-    completed: 0,
-    index: todos.length + 1
-  })
-  localStorage.setItem('todos', JSON.stringify(todos))
-  todoList.innerHTML = ''
-  displayToDoList()
-  inputField.value = ''
-  }
-})
-
+const inputField = document.querySelector('#inputField');
+const todoList = document.querySelector('#todoList');
 function displayToDoList() {
-  let data = localStorage.getItem('todos') 
+  const data = localStorage.getItem('todos');
   if (data) {
-    JSON.parse(data).forEach(todo => {
-      const li = document.createElement('li')
-      li.classList.add('list-group-item')
-      let text = `<div class="content" id="${todo.index}">  
-			<div class="d-flex">
-			<input class="form-check-input" type="checkbox" value="" ${todo.completed ? 'checked' : ''} id="defaultCheck1">
-        <div class="description ${todo.completed ? 'checked' : ''}" contenteditable="${!todo.completed}">${todo.description}</div> 
-			</div>
+    JSON.parse(data).forEach((todo) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item');
+      const text = `<div class="content" id="${todo.index}"><div class="d-flex"><input class="form-check-input" type="checkbox" value="" ${todo.completed ? 'checked' : ''} id="defaultCheck1">
+        <div class="description ${todo.completed ? 'checked' : ''}" contenteditable="${!todo.completed}">${todo.description}</div></div>
         <div  class="delete"><i class="fas fa-trash mt-2"></i></div>
-       </div>`
-      li.innerHTML = text
-      todoList.appendChild(li)
-      })
+       </div>`;
+      li.innerHTML = text;
+      todoList.appendChild(li);
+    });
   }
- 
+
+  const todos = [
+    {
+      description: 'wash my hair',
+      index: 1,
+      completed: 0,
+    },
+    {
+      description: 'sleep',
+      index: 2,
+      completed: 0,
+    },
+  ];
+  inputField.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      todos.push({
+        description: inputField.value,
+        completed: 0,
+        index: todos.length + 1,
+      });
+      localStorage.setItem('todos', JSON.stringify(todos));
+      todoList.innerHTML = '';
+      displayToDoList();
+      inputField.value = '';
+    }
+  });
 }
-displayToDoList()
+displayToDoList();
